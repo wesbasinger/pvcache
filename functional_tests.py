4d226c0baabf329fd0b2daf9f06d6aa0096e1e70
Wes Basinger <wbasinger@villagetechschools.org>
2015-12-31 18:37:31 -0600

    Wrote the test into classes, with the setup and teardown methods.

--- functional_tests.py
+++ functional_tests.py
@@ -1,30 +1,45 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
 
-# Cody has heard about a cool new online private geocaching app.
-# He goes to check out its homepage
-browser.get('http://localhost:8000')
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
 
-# He notices the page title and header mention VT Caching
-assert 'VT Caching' in browser.title
+	def tearDown(self):
+		self.browser.quit()
 
-# He then is able to view listings for several different
-# geocaches in the area.
+	def test_can_view_index_page(self):
 
-# When he clicks on one of the listings, it shows all the
-# important information for that particular cache - which
-# includes a title, a description, latitude, longitude, and
-# user logs.
+		# Cody has heard about a cool new online private 
+		# geocaching app.
+		# He goes to check out its homepage
+		self.browser.get('http://localhost:8000')
 
-# He is also given the option to download the information
-# in a GPX file that can be imported to his device.
+		# He notices the page title and header mention VT Caching
+		self.assertIn('VT Caching', self.browser.title)
+		
+		# He then is able to view listings for several different
+		# geocaches in the area.
 
-# Once he finds the geocache, he is able to create a log entry.
+		# When he clicks on one of the listings, it shows all the
+		# important information for that particular cache - which
+		# includes a title, a description, latitude, 
+		# longitude, and user logs.
 
-# After he submits the log entry, the page refreshes and he is
-# able to see that his entry is the latest.
+		# He is also given the option to download the information
+		# in a GPX file that can be imported to his device.
 
-# When Cody logs in as an admin user, he is able to add
-# geocaches of his own. 
+		# Once he finds the geocache, he is able to 
+		# create a log entry.
 
+		# After he submits the log entry, the page 
+		# refreshes and he is
+		# able to see that his entry is the latest.
+
+		# When Cody logs in as an admin user, he is able to add
+		# geocaches of his own. 
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
