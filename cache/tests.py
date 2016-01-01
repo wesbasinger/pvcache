7607011620afafc07031a54656f7210a81fcb394
Wes Basinger <wbasinger@villagetechschools.org>
2016-01-01 09:19:41 -0600

    Basic view now returns minimal HTML.

--- cache/tests.py
+++ cache/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
 from cache.views import index
 
 class IndexTest(TestCase):
@@ -7,3 +9,10 @@ class IndexTest(TestCase):
 	def test_root_url_resolves_to_index_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, index)
+
+	def test_index_returns_correct_html(self):
+		request = HttpRequest()
+		response = index(request)
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn(b'<title>VT Caching</title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
