65b3a6244409bc66fe72a0cd10bfbdc2710f33c3
Wes Basinger <wbasinger@villagetechschools.org>
2016-01-02 21:13:21 -0600

    Making the leap, foreign key field added to the Log model.  Now I've got to figure out how to write tests.

--- cache/models.py
+++ cache/models.py
@@ -6,5 +6,12 @@ class Geocache(models.Model):
 	latitude = models.FloatField(default=00.000000)
 	longitude = models.FloatField(default=00.000000)
 
+	def __str__(self):
+		return self.title
+
 class Log(models.Model):
 	text = models.TextField(default="None Provided")
+	geocache = models.ForeignKey(Geocache, on_delete=models.CASCADE, default=None)
+
+	def __str__(self):
+		return self.text
