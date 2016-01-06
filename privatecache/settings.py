97a63ea38efe7399657d0e4639fd66ccab0e9dbf
Wes Basinger <wbasinger@villagetechschools.org>
2016-01-05 19:10:39 -0600

    Moved db outside main directory.

--- privatecache/settings.py
+++ privatecache/settings.py
@@ -75,10 +75,13 @@ WSGI_APPLICATION = 'privatecache.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../geodata/db.sqlite3'),
     }
 }
 
