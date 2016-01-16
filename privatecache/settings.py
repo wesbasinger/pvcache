4643d7a1b0c39b222d2d99e7083e6996c98cef93
Wes Basinger <wbasinger@villagetechschools.org>
2016-01-16 09:22:10 -0600

    Switched to postgres on the local machine

--- privatecache/settings.py
+++ privatecache/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'privatecache.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../geodata/db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vtcaching',
+		'USER': 'wes',
+		'PASSWORD': 'annie510',
+		'HOST': 'localhost',
+		'PORT': '',
     }
 }
 
